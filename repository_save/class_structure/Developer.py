ef0d9d6bd185bbdff430bd0ffd4a7dc19d7be74c
DerekSomervilleUofG <=>
2024-07-22 11:24:31 +0100

    Developer email login

--- repository_save/class_structure/Developer.py
+++ repository_save/class_structure/Developer.py
@@ -4,8 +4,8 @@ class Developer(SuperClass):
     
     control_populate = ControlPopulate()
 
-    def __init__(self, name, primary_key=0):
-        super().__init__(name, primary_key)
+    def __init__(self, name, email, login, primary_key=0):
+        super().__init__(name, email, login, primary_key)
         self.populate_structure = self.control_populate.get_populate_developer()
         self.populate_structure.add_structure(self)
 
