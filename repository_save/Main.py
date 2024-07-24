bead6ff54fbc3b5909301432a4a23ba9a17e9e40
DerekSomervilleUofG <=>
2024-07-24 15:06:28 +0100

    database create

--- repository_save/Main.py
+++ repository_save/Main.py
@@ -1,11 +1,14 @@
 from repository_save.data_source.DBExecuteSQL import DBExecuteSQL
 from utility.ReadWriteFile import ReadWriteFile
+from repository_save.data_source.DatabaseCreate import DatabaseCreate
 import sys
 
 db_execute_sql = DBExecuteSQL()
 read_write_file = ReadWriteFile()
 
 class Main():
+    
+    database_create = DatabaseCreate()
     
     def before_processing(self):
         pass
@@ -21,6 +24,7 @@ class Main():
         for database in read_write_file.get_list_from_directory(directory):
             if ".db" in database:
                 db_execute_sql.set_db_file_name(directory + database)
+                self.database_create.setup()
                 self.process_database(database)
         self.after_processing()
 
